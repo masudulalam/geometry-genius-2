29000fab07d8e66133f8959b822199704433af41
masudulalam <masuudulalam@gmail.com>
2024-04-09 02:21:31 +0600

    added triangle area calculation

---
+++ script/triangle-calculate.js
@@ -0,0 +1,20 @@
+function calculateTriangleArea (){
+    // get triangle base value
+    const triangleBaseInput = document.getElementById('triangle-base');
+    const triangleBaseText = triangleBaseInput.value;
+    const base = parseFloat(triangleBaseText);
+    // console.log(base);
+
+    // get triangle height value
+    const triangleHeightInput = document.getElementById('triangle-height');
+    const triangleHeightText = triangleHeightInput.value;
+    const height = parseFloat(triangleHeightText);
+    // console.log(height);
+
+    // calculate triangle area
+    const area = 0.5 * base * height;
+    console.log(area);
+
+    // display triangle area
+    document.getElementById('display-triangle-area').innerText = area;
+}
