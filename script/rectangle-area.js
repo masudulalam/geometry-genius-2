9fa6e5b95c6f48fe9386d46b382eda11d8dde429
masudulalam <masuudulalam@gmail.com>
2024-04-09 18:58:47 +0600

    add rectangle area calculation

--- script/rectangle-area.js
+++ script/rectangle-area.js
@@ -1,3 +1,20 @@
 function calculateRectangleArea () {
-    console.log('from inner function');
+    // get rectangle width
+    const rectangleWidthInput = document.getElementById('rectangle-width');
+    const rectangleWidthText = rectangleWidthInput.value;
+    const rectangleWidth = parseFloat(rectangleWidthText);
+    // console.log(typeof rectangleWidth, rectangleWidth);
+
+    // get rectangle length
+    const rectangleLengthInput = document.getElementById('rectangle-height');
+    const rectangleLengthText = rectangleLengthInput.value;
+    const rectangleLength = parseFloat(rectangleLengthText);
+    // console.log(rectangleLength); 
+
+    // calculate rectangle area
+    const rectangleArea = rectangleWidth * rectangleLength;
+    console.log(rectangleArea);
+
+    // display rectangle area
+    document.getElementById('display-rectangle-area').innerText = rectangleArea;
 }
